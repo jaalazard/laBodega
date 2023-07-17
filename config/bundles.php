ff51df34792639cff6ff4fc8a2c5d090b42a777d
jaalazard <ja.alazard@proton.me>
2023-07-17 15:56:43 +0200

    config de vich

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\UX\Typed\TypedBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\UX\Translator\UxTranslatorBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
